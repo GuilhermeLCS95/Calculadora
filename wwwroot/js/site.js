4324f666a72eb6529e4b7bba710e2398a5863bee
GuilhermeLCS <guilhermeluiscdasilva@hotmail.com>
2023-04-03 04:02:12 -0300

    Quase pronto

---
+++ wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function insert(num) {
+   var num2 = window.document.getElementById('result').innerHTML;
+    window.document.getElementById('result').innerHTML = num2 + num;
+}
+function clean() {
+    window.document.getElementById('result').innerHTML = "";
+}
+function backspace() {
+    var lastChar = window.document.getElementById('result').innerHTML;
+    window.document.getElementById('result').innerHTML = lastChar.substring(0, lastChar.length - 1);
+}
+function calc() {
+    var outcome = window.document.getElementById('result').innerHTML;
+
+    if (outcome) {
+        document.getElementById('result').innerHTML = eval(outcome);
+    }
+    else {
+        window.document.getElementById('result').innerHTML = "It's impossible to calculate.";
+    }
+}
